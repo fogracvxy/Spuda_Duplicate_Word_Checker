ddb87a02a4c20c89dc4ee966dee5844524fc22a7
AtomicTempest <marn1002000@gmail.com>
2020-07-02 12:33:30 +0200

    Fixed code

--- src/com/Spuda/Main.java
+++ src/com/Spuda/Main.java
@@ -1,5 +1,6 @@
 //Spuda checker duplih rijeci
 package com.Spuda;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
@@ -11,7 +12,7 @@ class test {
         int count;
         String stringInput;
         int exit = 0;
-        while(true) {
+        while (true) {
             System.out.println("Enter words with space");
             stringInput = input.nextLine();
 
@@ -19,13 +20,13 @@ class test {
             stringInput = stringInput.toLowerCase();
 
             //Razdvajanje stringa na rijeci
-            String words[] = stringInput.split(" ");
+            String[] words = stringInput.split(" ");
 
             System.out.println("Duplicate words : ");
-            for(int i = 0; i < words.length; i++) {
+            for (int i = 0; i < words.length; i++) {
                 count = 1;
-                for(int j = i+1; j < words.length; j++) {
-                    if(words[i].equals(words[j])) {
+                for (int j = i + 1; j < words.length; j++) {
+                    if (words[i].equals(words[j])) {
                         count++;
                         //stavljanje na 0 da se ne vraca na vec iste rijeci
                         words[j] = "0";
@@ -33,21 +34,18 @@ class test {
                 }
 
                 //prikazanje duplih rijeci ako je broj veci od 1
-                if(count > 1 && words[i] != "0")
+                if (count > 1 && !words[i].equals("0"))
                     System.out.println(words[i]);
             }
-            try
-            {
+            try {
                 System.out.println("Want to turn off program? Type: 1");
                 exit = input.nextInt();
-            }
-            catch(InputMismatchException exception)
-            {
+            } catch (InputMismatchException exception) {
                 System.out.println("This is not an integer");
             }
             input.nextLine();
-            if (exit == 1){
-                System.out.println("Gasim program za 3 sekunde");
+            if (exit == 1) {
+                System.out.println("Turning program off in 3 seconds! BYE! Author: Marin Spudic");
                 TimeUnit.SECONDS.sleep(3);
                 break;
             } else {
