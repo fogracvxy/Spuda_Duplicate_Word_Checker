f51c6d66e3692e66afe6043d46831c447dac7bf9
fogracvxy <marn1002000@gmail.com>
2024-07-08 18:04:55 +0200

    Updated code to more robust one with Sets and Maps

--- src/com/Spuda/Main.java
+++ src/com/Spuda/Main.java
@@ -4,54 +4,74 @@ package com.Spuda;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
-
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 class test {
 
     public static void main(String[] args) throws InterruptedException {
         Scanner input = new Scanner(System.in);
-        int count;
-        String stringInput;
-        int exit = 0;
         while (true) {
             System.out.println("Enter words with space");
-            stringInput = input.nextLine();
+            String stringInput = input.nextLine().toLowerCase();
+            String[] words = stringInput.split("\\s+");
 
-            //pretvaranje stringa u mala slova radi lakse usporedbe
-            stringInput = stringInput.toLowerCase();
+            String duplicates = findDuplicates(words);
+            String nonDuplicates = findNonDuplicates(words);
 
-            //Razdvajanje stringa na rijeci
-            String[] words = stringInput.split(" ");
+            System.out.println("Duplicate words: " + duplicates);
+            System.out.println("Non-duplicate words: " + nonDuplicates);
 
-            System.out.println("Duplicate words : ");
-            for (int i = 0; i < words.length; i++) {
-                count = 1;
-                for (int j = i + 1; j < words.length; j++) {
-                    if (words[i].equals(words[j])) {
-                        count++;
-                        //stavljanje na 0 da se ne vraca na vec iste rijeci
-                        words[j] = "0";
-                    }
-                }
-
-                //prikazanje duplih rijeci ako je broj veci od 1
-                if (count > 1 && !words[i].equals("0"))
-                    System.out.println(words[i]);
-            }
-            try {
-                System.out.println("Want to turn off program? Type: 1");
-                exit = input.nextInt();
-            } catch (InputMismatchException exception) {
-                System.out.println("This is not an integer");
-            }
-            input.nextLine();
-            if (exit == 1) {
+            if (shouldExit(input)) {
                 System.out.println("Turning program off in 3 seconds! BYE! Author: Marin Spudic");
                 TimeUnit.SECONDS.sleep(3);
                 break;
-            } else {
-
-                System.out.println("Enter words again:");
             }
+        }
+    }
+
+    private static String findDuplicates(String[] words) {
+        Set<String> uniqueWords = new HashSet<>();
+        Set<String> duplicates = new HashSet<>();
+
+        for (String word : words) {
+            if (!uniqueWords.add(word)) {
+                duplicates.add(word);
+            }
+        }
+
+        return String.join(" ", duplicates);
+    }
+
+    private static String findNonDuplicates(String[] words) {
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        List<String> nonDuplicates = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                nonDuplicates.add(entry.getKey());
+            }
+        }
+
+        return String.join(" ", nonDuplicates);
+    }
+
+    private static boolean shouldExit(Scanner input) {
+        System.out.println("\nWant to turn off program? Type: 1");
+        try {
+            int exit = input.nextInt();
+            input.nextLine();
+            return exit == 1;
+        } catch (InputMismatchException exception) {
+            System.out.println("This is not an integer");
+            input.nextLine();
+            return false;
         }
     }
 
